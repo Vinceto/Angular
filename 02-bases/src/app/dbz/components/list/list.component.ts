9ff729bf974fdd8572eb40354158b60f9f6f180a
Ramon Bernabe Munoz Navarrete <ramon.munozn@idiem.cl>
2025-01-30 16:59:06 -0300

    Seccion 4 - angular cap 63 - 67

--- 02-bases/src/app/dbz/components/list/list.component.ts
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.css'
+  styleUrl: './list.component.css',
+  imports: [CommonModule],
 })
 export class ListComponent {
+  @Input()
+  public characterList: Character[] = [
+    {
+      name: 'Trunks',
+      power: 10
+    }
+  ]
+
 }
--- 02-bases/src/app/dbz/pages/main-page.component.ts
+++ 02-bases/src/app/dbz/pages/main-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DbzModule } from "../dbz.module";
 import { ListComponent } from "../components/list/list.component";
 import { AddCharacterComponent } from "../components/add-character/add-character.component"
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -11,4 +12,21 @@ import { AddCharacterComponent } from "../components/add-character/add-character
 
 export class MainPageComponent {
 
+  public characters: Character[] = [{
+    name: 'Krillin',
+    power: 1000
+  },
+  {
+    name: 'Goku',
+    power: 9500
+  },
+  {
+    name: 'Vegeta',
+    power: 7500
+  }];
+
+  onNewCharacter(character:Character):void{
+    console.log('Main Page');
+    console.log(character);
+  }
 }
